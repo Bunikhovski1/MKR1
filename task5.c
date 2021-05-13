55527e67688d34735190dd47fa7c996a25600aa4
Bunikhovski1 <79472947+Bunikhovski1@users.noreply.github.com>
2021-05-13 15:16:02 +0300

    Add files via upload

---
+++ task5.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+
+
+
+int main() {
+  int method, counter = 0, age;
+  printf("\nЩо будемо робити ?\n");
+  printf("*******************************************\n");
+  printf("Запустити програму - 1\n");
+  printf("Вийти з програми - 2\n");
+  printf("*******************************************\n");
+  while(1) {
+    if (counter == 0) {
+      printf("\nВвести: ");
+      scanf("%d", &method);
+      counter += 1;
+    }
+    else {
+      printf("\n\nВи можете спробувати ще раз - 1 || вийти з програми - 2\n");
+      printf("Ввести: ");
+      scanf("%d", &method);
+    }
+    if (method == 1) {
+       printf("Введіть ваш вік: ");
+       scanf("%d", &age);
+       if (age < 0) {
+         printf("Ви ввели некоректні дані");
+       }
+       else if (age <= 10) printf("Ви - дитина");
+       else if (age <= 15) printf("Ви - підліток");
+       else if (age <= 20) printf("Ви - юнак");
+       else if (age <= 30) printf("Ви - молода людина");
+       else printf("Ви - доросла людина");
+    }
+    else {
+      break;
+    }
+  }
+
+  return 0;
+}  
