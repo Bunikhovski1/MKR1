8e04309640a4fbb5eba24203e6006c08c445829a
Bunikhovski1 <79472947+Bunikhovski1@users.noreply.github.com>
2021-05-13 15:15:38 +0300

    Add files via upload

---
+++ task4.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+  float result; //Оголення змінної
+
+  //Обчислення
+  result  = ((sqrt(2) + 2 + pow(2, 2)) / (sqrt(3) + 3 + pow(3, 2)))  + ((sqrt(4) + 4 +pow(4, 2)) / (sqrt(8) + 8 + pow(8,2)) );
+
+  // Виведення
+  printf("\n*******");
+  printf("Результат");
+  printf("*******\n");
+  printf("Відповідь: ");
+  printf("%.2f\n", result);
+
+  return 0;
+} 
